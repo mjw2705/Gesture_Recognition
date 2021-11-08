d202d3fe86b43654566907d3ae722a4a9d12396b
jiwon <mjw2705@naver.com>
2021-11-08 21:28:42 +0900

    gesture 영역 내 detect

--- gesture_test.py
+++ gesture_test.py
@@ -5,7 +5,6 @@ import math
 import cv2
 import numpy as np
 import mediapipe as mp
-from tensorflow.keras.models import load_model
 import onnxruntime
 import utils
 
@@ -33,11 +32,12 @@ before_x = 0
 before_y = 0
 count = 0
 seq_joint = []
-
 frames = []
 
 frame_w = 640
 frame_h = 480
+
+hand_data = dict()
 
 # 좌표값 저장
 csv_path = 'point_history.csv'
@@ -54,44 +54,26 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_w)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_h)
 cap.set(cv2.CAP_PROP_FPS, 120)
 
+sx =0
+sy =0
+ex =0
+ey =0
+
 while cap.isOpened():
     ret, image = cap.read()
+    boxes, labels, probs = utils.face_detect(image, face_session, input_name)
 
-    image = cv2.flip(image, 1)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    hands = hand.process(image)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-    # face detect
-    img1 = cv2.resize(image, (320, 240))
-    image_mean = np.array([127, 127, 127])
-    img1 = (img1 - image_mean) / 128
-    img1 = np.transpose(img1, [2, 0, 1])
-    img1 = np.expand_dims(img1, axis=0)
-    img1 = img1.astype(np.float32)
-
-    confidences, boxes = face_session.run(None, {input_name: img1})
-    boxes, labels, probs = utils.predict(image.shape[1], image.shape[0], confidences, boxes, 0.9)
     try:
-        boxe_width = [int(w[2] - w[0]) for w in boxes]
-        box_idx = np.argmax(boxe_width)
-        box = boxes[box_idx, :]
-        # add box margin
-        box_margin = int((box[2] - box[0]) / 2)
-        box = [box[0] - box_margin, box[1] - box_margin, box[2] + box_margin, box[3] + box_margin]
-
-        width = box[2] - box[0]
-        height = box[3] - box[1]
-
-        new_x1 = box[0] - width
-        new_x2 = box[2] + width
-        new_y2 = box[3] + height * 2
+        # calc box position
+        new_x1, new_x2, new_y2, box, sx, sy, ex, ey = utils.box_pos(boxes)
 
         # draw gesture region
         cv2.rectangle(image, (new_x1, box[1]), (new_x2, new_y2), (0, 0, 255), 1, cv2.LINE_AA)
-
-        img_roi = image[box[1]:new_y2, new_x1:new_x2].copy()
-        h, w, _ = img_roi.shape
+        image = cv2.flip(image, 1)
+        img_roi = image[sy:ey, sx:ex].copy()
+        frame_h, frame_w, _ = img_roi.shape
+        img_roi = cv2.cvtColor(img_roi, cv2.COLOR_BGR2RGB)
+        hands = hand.process(img_roi)
 
         # hand
         if hands.multi_hand_landmarks:
@@ -103,44 +85,23 @@ while cap.isOpened():
 
                 for j, lm in enumerate(res.landmark):
                     joint[j] = [lm.x, lm.y, lm.z, lm.visibility]
-                    abs_joint[j] = [int(lm.x * frame_w), int(lm.y * frame_h)]
+                    abs_joint[j] = [int(lm.x * frame_w + sx), int(lm.y * frame_h + sy)]
 
-                seq_joint.append(joint)
+                for lm in abs_joint:
+                    # 이미지에 손 랜드마크 그림
+                    cv2.circle(image, (int(lm[0]), int(lm[1])), 2, (0, 255, 0), -1, cv2.LINE_AA)
+                    seq_joint.append(joint)
 
-                # 제스쳐 영역 내에서만 제스쳐 가능
-                if np.any(abs_joint[:, 0] >= new_x1) and np.any(abs_joint[:, 0] <= new_x2) \
-                        and np.any(abs_joint[:, 1] > box[1]) and np.any(abs_joint[:, 1] < new_y2):
-
-                    # Compute angles between joints
-                    v1 = joint[[0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0, 17, 18, 19], :3]  # Parent joint
-                    v2 = joint[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], :3]  # Child joint
-                    v = v2 - v1  # [20, 3]
-                    # Normalize v
-                    v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
-
-                    # Get angle using arcos of dot product
-                    angle = np.arccos(np.einsum('nt,nt->n',
-                                                v[[0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18], :],
-                                                v[[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19], :]))  # [15,]
-
-                    angle = np.degrees(angle)  # Convert radian to degree
+                    # 손가락 포인트의 각도 계산
+                    angle = utils.calc_angle(joint)
                     d = np.concatenate([joint.flatten(), angle])
 
-                    mp_drawing.draw_landmarks(image, res, mp_hands.HAND_CONNECTIONS)
-
-                    input_data = np.expand_dims(np.array(d, dtype=np.float32), axis=0)
-
-                    input_data = input_data if isinstance(input_data, list) else [input_data]
-                    feed = dict([(input.name, input_data[n]) for n, input in enumerate(gesture_session.get_inputs())])
-
-                    y_pred = gesture_session.run(None, feed)[0].squeeze()
-                    i_pred = int(np.argmax(y_pred))
-
-                    conf = y_pred[i_pred]
-
+                    # 예측 정확도 및 예측 인덱스 뽑기
+                    conf, i_pred = utils.calc_predict(d, gesture_session)
                     if conf < 0.9:
                         continue
 
+                    # 제스처 이름 받아옴
                     action = actions[i_pred]
 
                     # swipe 알고리즘
@@ -167,32 +128,33 @@ while cap.isOpened():
                                     action = 'down'
                                     swipe_seq = []
 
+                    elif action != 'swipe':
+                        swipe_seq = []
+
                     # grab 각도 계산
                     if action == 'grab':
                         if label == 'Left':
-                            angle2 = (math.degrees(math.atan2(joint[3][1] - joint[17][1],
+                            grab_angle = (math.degrees(math.atan2(joint[3][1] - joint[17][1],
                                                               joint[3][0] - joint[17][0])))
                         else:
-                            angle2 = (math.degrees(math.atan2(joint[17][1] - joint[3][1],
+                            grab_angle = (math.degrees(math.atan2(joint[17][1] - joint[3][1],
                                                               joint[17][0] - joint[3][0])))
 
                         utils.draw_timeline(image, angle, abs_joint)
 
-                    if action != 'swipe':
-                        swipe_seq = []
-
-                    print(count, action)
                     cv2.putText(image, f'{action.upper()}',
                                 org=(int(abs_joint[0][0]), int(abs_joint[0][1] + 20)),
                                 fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
 
                     if action == 'grab':
-                        utils.logging_csv(csv_path, count, action, angle2, abs_joint)
+                        utils.logging_csv(csv_path, count, action, grab_angle, abs_joint)
+                        utils.save_dict(hand_data, abs_joint, action, grab_angle)
                     else:
                         utils.logging_csv(csv_path, count, action, 'None', abs_joint)
+                        utils.save_dict(hand_data, abs_joint, action, 'None')
 
-                else:
-                    continue
+                print(count, action)
+                print(hand_data)
 
         else:
             # 보간
@@ -216,7 +178,6 @@ while cap.isOpened():
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    delay = 1000 / fps
 
     cv2.putText(image, f"FPS : {fps:.2f}", (10, 50), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0), 1)
     cv2.imshow('img', image)
